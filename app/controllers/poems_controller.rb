d3a9dabd5ec128d76edcdd2380633a0b086fc318
Gale Harrington <gale.harrington19@gmail.com>
2018-01-23 10:51:32 -0800

    Remove bang from shuffle

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -3,8 +3,7 @@ class PoemsController < ApplicationController
   def index
     #method only works for a 1-word query
     if params[:query]
-      poems = Poem.search_poems(params[:query])
-      poems.shuffle!
+      poems = Poem.search_poems(params[:query]).shuffle
       render status: :ok, json: poems
     else
       #generate random poems if no query given
